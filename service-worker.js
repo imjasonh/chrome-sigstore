ea3baec63d525fee116bc4ddb8c83be17f96755e
Jason Hall <jason@chainguard.dev>
2023-06-09 21:23:43 -0400

    initial commit
    Signed-off-by: Jason Hall <jason@chainguard.dev>

---
+++ service-worker.js
@@ -0,0 +1,14 @@
+chrome.tabs.onUpdated.addListener(({ tabID, changeInfo, tab }) => {
+    ['Chainguard Authentication', 'Sigstore Authentication'].forEach((title) => {
+        chrome.tabs.query({
+            title: title,
+            url: "http://localhost:*/*"
+        }).then((tabs) => {
+            console.log(`tabs ${tabs.url}`);
+            tabs.forEach((tab) => {
+                console.log(`closing tab ${tab.id} with title ${tab.title} and url ${tab.url}`);
+                chrome.tabs.remove(tab.id);
+            });
+        });
+    });
+});
