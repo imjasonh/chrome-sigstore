4d83ce920fa4c5088713263da446cef3842e9884
Jason Hall <jason@chainguard.dev>
2023-08-10 09:50:03 -0400

    drop options for now
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- service-worker.js
+++ service-worker.js
@@ -5,25 +5,18 @@ chrome.tabs.onUpdated.addListener(({ tabID, changeInfo, tab }) => {
             url: "http://localhost:*/*"
         }).then((tabs) => {
             tabs.forEach((tab) => {
-                chrome.storage.sync.get(
-                    { timeToClose: '100' },
-                    (items) => {
-                        setTimeout(() => {
-                            console.log(`closing tab ${tab.id} with title ${tab.title} and url ${tab.url}`);
-                            chrome.tabs.remove(tab.id, () => {
-                                chrome.storage.sync.get(["count"]).then((result) => {
-                                    console.log("Value currently is " + result.count);
-                                    let count = parseInt(result.count || 0) + 1;
-                                    chrome.storage.sync.set({ count: count }).then(() => {
-                                        console.log("Value is set to " + count);
-                                        chrome.action.setBadgeText({ text: count.toString() });
-                                        chrome.action.setBadgeBackgroundColor({ color: "#221F5C" });
-                                    });
-                                });
-                            });
-                        }, items.timeToClose);
-                    }
-                );
+                console.log(`closing tab ${tab.id} with title ${tab.title} and url ${tab.url}`);
+                chrome.tabs.remove(tab.id, () => {
+                    chrome.storage.sync.get(["count"]).then((result) => {
+                        console.log("Value currently is " + result.count);
+                        let count = parseInt(result.count || 0) + 1;
+                        chrome.storage.sync.set({ count: count }).then(() => {
+                            console.log("Value is set to " + count);
+                            chrome.action.setBadgeText({ text: count.toString() });
+                            chrome.action.setBadgeBackgroundColor({ color: "#221F5C" });
+                        });
+                    });
+                });
             });
         });
     });
