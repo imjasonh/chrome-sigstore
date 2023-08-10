b4f10c6e2c6ba3ed634d3bc854d7d6e7e7e79737
Jason Hall <jason@chainguard.dev>
2023-08-10 09:17:17 -0400

    add badge with tab count
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- service-worker.js
+++ service-worker.js
@@ -10,7 +10,17 @@ chrome.tabs.onUpdated.addListener(({ tabID, changeInfo, tab }) => {
                     (items) => {
                         setTimeout(() => {
                             console.log(`closing tab ${tab.id} with title ${tab.title} and url ${tab.url}`);
-                            chrome.tabs.remove(tab.id);
+                            chrome.tabs.remove(tab.id, () => {
+                                chrome.storage.sync.get(["count"]).then((result) => {
+                                    console.log("Value currently is " + result.count);
+                                    let count = parseInt(result.count || 0) + 1;
+                                    chrome.storage.sync.set({ count: count }).then(() => {
+                                        console.log("Value is set to " + count);
+                                        chrome.action.setBadgeText({ text: count.toString() });
+                                        chrome.action.setBadgeBackgroundColor({ color: "#221F5C" });
+                                    });
+                                });
+                            });
                         }, items.timeToClose);
                     }
                 );
